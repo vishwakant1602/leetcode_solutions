c1e63ce4de46799c3c76de0bb5e2161cac1039d1
Vishwakant <121593799+vishwakant1602@users.noreply.github.com>
2026-07-30 21:36:13 +0530

    Time: 52 ms (80.72%), Space: 29.5 MB (67.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,6 +4,8 @@ class Solution:
         l,r=0, len(height)-1
 
         while l<r:
+            #for l in range(len(height)):
+                #for  in range(l+1, len(height)):
             area = (r-l) * min(height[l],height[r])
             res = max(res, area)
 
@@ -12,4 +14,5 @@ class Solution:
             else:
                 r-=1
         return res
+
             
