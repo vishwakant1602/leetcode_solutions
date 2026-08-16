fe28ce747580072c4286c989a59ff8436a839656
Vishwakant <121593799+vishwakant1602@users.noreply.github.com>
2026-08-16 11:12:22 +0530

    Time: 2 ms (58.76%), Space: 12 MB (30.24%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> counter;
+
+        for (char c : magazine){
+            counter[c]++;
+        }
+
+        for (char c : ransomNote){
+            if(counter[c]==0){
+                return false;
+            }
+            counter[c]--;
+        }
+        return true;  
+    }
+};
